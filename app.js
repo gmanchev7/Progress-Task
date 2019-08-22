27e53643cbf453536bdb1ab72e0e6f51f8d8168e
gmanchev17 <mancheew@abv.bg>
2019-08-22 04:51:36 +0300

    Part1

---
+++ app.js
@@ -0,0 +1,11 @@
+angular.module("todoApp", []).controller("AppController", function () {
+  var information = this;
+  information.todos = data;
+
+  information.addTodo = function () {
+    console.log("asd");
+    console.log(information.todos);
+  };
+
+});
+
---
+++ features/expandCollapse.js
@@ -0,0 +1,7 @@
+var expandCollapseApp = angular.module('expandCollapseApp', ['ngAnimate']);
+
+expandCollapseApp.controller('expandCollapseCtrl', function ($scope) {
+        $scope.active = true;
+        $scope.active1 = true;
+        
+});
